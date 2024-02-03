e3f98e021142c1853082693103489c19a2568495
Raeshana <rsookhoo@andrew.cmu.edu>
2024-02-02 22:04:47 -0500

    reset format

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    [ContextMenu("Next")]
+    public void Next()
+    {
+        int current = SceneManager.GetActiveScene().buildIndex;
+        int next = current + 1;
+
+        SceneManager.LoadScene(next, LoadSceneMode.Single);
+    }
+
+    [ContextMenu("Back")]
+    public void Back()
+    {
+        int current = SceneManager.GetActiveScene().buildIndex;
+        int back = current - 1;
+
+        SceneManager.LoadScene(back, LoadSceneMode.Single);
+    }
+}
